3ea0c6eee90f84833ee1dfb3bb1b1154548a70ec
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-29 21:32:08 +0100

    create GET route for all questions

--- server/routes/QuestionRoutes.js
+++ server/routes/QuestionRoutes.js
@@ -27,10 +27,46 @@ router.post('/', async (req, res) => {
                     message: 'Question could not be added',
                 })
             })
-    } catch (error) {
+    } catch (err) {
         res.status(500).send({
             status: false,
             message: 'Server error while adding question',
         }); 
     }
 });
+
+// Get all questions with their answers
+// GET /api/questions - GET ALL QUESTIONS
+router.get('/', async (req, res) => {
+    try {
+        // Perform a MongoDB 'join' query to fetch all answers for questions based on the questionId field
+        await questionDB
+            .aggregate([
+                {
+                    $lookup: {
+                        from: 'answers',
+                        localField: '_id',
+                        foreignField: 'questionId',
+                        as: 'allAnswers',
+                    },
+                },
+            ])
+            .exec()
+            .then((doc) => {
+                res.status(200).send(doc);
+            })
+            .catch((error) => { //! why is this error vs err
+                res.status(500).send({
+                    status: false,
+                    message: 'Unable to get the questions',
+                });
+            });
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            message: 'Server error while getting questions',
+        });
+    }
+});
+
+module.exports = router;
