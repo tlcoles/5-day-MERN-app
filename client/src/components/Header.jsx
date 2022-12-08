017e32223dd7bcab7d50d1bc84a40b08bfef0374
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-12-08 20:22:40 +0100

    import icons, avatar, and buttons from @mui/icons-material

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { AssignmentTurnedInOutlinedIcon, CloseIcon, NotificationsOutlinedIcon, FeaturedPlayListOutlinedIcon,PeopleAltOutlinedIcon, SearchIcon, HomeIcon, ExpandMoreIcon } from '@mui/icons-material/';
+
+import { Avatar, Button } from '@mui/material';
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className="flex items-center bg-white sticky z-50 shadow-sm top-0 justify-center p-[3px]">
+      <div className="flex">
+          <span className="text-orange-500 text-lg font-semibold mr-5">5DayMernApp</span>
+      </div>
+      <div>
+        Icons
+      </div>
+      <div>
+        Avatar & Button
+      </div>
     </div>
   )
 };
