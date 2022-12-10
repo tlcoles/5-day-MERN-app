d51e437d61f3d56ed78095d06a68f72717c68ccf
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-12-10 11:09:50 +0100

    install, configure Google Firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyjbHVAFmO1AfXxVjT4M3tmUgHTXXiGfg",
+  authDomain: "daymernapp.firebaseapp.com",
+  projectId: "daymernapp",
+  storageBucket: "daymernapp.appspot.com",
+  messagingSenderId: "525082622735",
+  appId: "1:525082622735:web:da623bb501938b289c9c7a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export { auth, provider }; 
