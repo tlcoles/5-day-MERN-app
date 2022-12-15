04cbdcf095cc70b36b20cb4db01a22099eb78dd1
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-12-15 22:25:39 +0100

    render login/home page using user auth state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect } from 'react';
+import Login from "./pages/Login";
 import Home from "./pages/Home";
+import { login, selectUser } from "../src/features/userSlice";
+import { auth } from "./firebase"
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            userName: authUser.displayName,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            uid: authUser.uid,
+          })
+        );
+      }
+    });
+  }, [dispatch]);
+  
+  return <div>{
+    user ? 
+    <Home /> : 
+    <Login />
+  }</div>;
 }
 
 export default App;
