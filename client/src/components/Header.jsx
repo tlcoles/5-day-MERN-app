abfdd9b77fb0fa989ef182fc1af63d1d6d905a33
Tammi <tlcoles@users.noreply.github.com>
2022-12-17 10:33:24 +0100

    add logout functionality to avatar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { auth } from "../firebase";
+import { useDispatch, useSelector } from "react-redux";
 import { 
   AssignmentTurnedInOutlined, 
   Close, 
@@ -13,6 +15,20 @@ import {
 import { Avatar } from '@mui/material';
 
 const Header = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  
+  const handleLogout = () => {
+    if(window.confirm("Are you sure you want to logout?")){
+      signOut(auth).then(() => {
+        dispatch(logout());
+        alert("Logged out")
+      }).catch(() => {
+        alert("Error logging out")
+        })
+      }
+  }
+  
   return (
     <div className="flex items-center bg-white sticky z-50 shadow-sm top-0 justify-center p-[3px]">
       <div className="flex">
@@ -40,8 +56,8 @@ const Header = () => {
         <input className="bg-transparent outline-none border-none" type="text" placeholder='Search...'/>
       </div>
       <div className="flex items-center ml-[25px]">
-        <span>
-          <Avatar/>
+        <span onClick={handleLogout}>
+          <Avatar src={user?.photo}/>
         </span>
         <button className="px-2 py-1 rounded-lg ml-1 bg-orange-300 hover:bg-orange-500">Add Question</button>
       </div>
