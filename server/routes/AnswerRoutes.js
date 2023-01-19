59a31b4fa27a0a5f9df29bac0aa405b754ff6b93
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2023-01-19 20:52:17 +0100

    insert line

--- server/routes/AnswerRoutes.js
+++ server/routes/AnswerRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Import our answer model
@@ -6,31 +7,31 @@ const answerDB = require('../models/AnswerModel');
 
 // Answer a question
 router.post('/', async (req, res) => {
-    try {
-        await answerDB
-        .create({
-            answer: req.body.answer,
-            questionId: req.body.questionId,
-            user: req.body.user,
-        })
-        .then(() => {
-            res.status(201).send({
-                status: true,
-                message: 'Answer added successfully',
-            });
-        })
-        .catch((err) => {
-            res.status(400).send({
-                status: false,
-                message: 'Error adding answer',
-            });
+  try {
+    await answerDB
+      .create({
+        answer: req.body.answer,
+        questionId: req.body.questionId,
+        user: req.body.user,
+      })
+      .then(() => {
+        res.status(201).send({
+          status: true,
+          message: 'Answer added successfully',
         });
-    } catch (err) {
-        res.status(500).send({
-            status: false,
-            message: 'Server error while adding answer',
+      })
+      .catch((err) => {
+        res.status(400).send({
+          status: false,
+          message: 'Error adding answer',
         });
-    }
+      });
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: 'Server error while adding answer',
+    });
+  }
 });
 
 module.exports = router;
