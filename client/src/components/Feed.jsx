b750d71a4c06148203f25d7c4d94be71766fb8dc
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2023-01-27 12:11:05 +0100

    fetch all posts into feed

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const fetchQuestions = async() => {
+    const res = await axios.get('/api/questions');
+    setPosts(res.data);
+  }
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="flex flex-col flex-1 p-2 md:flex-[0.6]">
-        Feed
+        {
+          posts.map((post,index) => (
+            <Post key={index} post={post}/>
+          ))
+        }
     </div>
   )
 };
